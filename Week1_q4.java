bef21d274a14745cdc2fbbc1d71283be0fb77dae
shanmuganathansameeksha-design <shanmuganathansameeksha@gmail.com>
2026-07-25 19:29:12 +0530

    Add files via upload

---
+++ Week1_q4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Factorial{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int fact=1;
+System.out.println("Enter a number:");
+ int n= sc.nextInt();
+for(int i=1;i<=n;i++){
+fact=fact*i;
+}
+System.out.println("Factorial:" + fact);
+}
+}
+
