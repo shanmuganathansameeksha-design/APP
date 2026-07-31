697249fab91aae8c8848162a280d7857be13d9ff
shanmuganathansameeksha-design <shanmuganathansameeksha@gmail.com>
2026-07-31 13:03:44 +0530

    Add files via upload

---
+++ Week2_q1.java
@@ -0,0 +1,16 @@
+class Student{
+String name;
+int rollno;
+String department;
+}
+public static void main(String[] args){
+Student s= new Student();
+s.name="Sameeksha";
+s.rollno=18;
+s.department="CSE";
+System.out.println("Student Details:");
+System.out.println("Name: " + s.name);
+System.out.println("Roll number: " + s.rollno);
+System.out.println("Department: " + s.department);
+}
+}
